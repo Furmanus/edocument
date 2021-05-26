a42d8791f1f666de601adfff62dbe5895e436116
Lukasz Lach <lukasz.lach@getresponse.com>
2021-05-27 00:14:12 +0200

    use express adapter, enable https

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,18 +1,37 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import { json } from 'body-parser';
 import helmet from 'helmet';
+import express from 'express';
+import http from 'http';
+import https from 'https';
+import appRoot from 'app-root-path';
 import expressSession from 'express-session';
 import { resolve } from 'path';
 import { AppModule } from './app.module';
 import MongoStore from 'connect-mongo';
+import { readFileSync } from 'fs';
 
-const { PORT, DATABASE_URL } = process.env;
+const appRootPath = appRoot.toString();
+const { PORT, HTTPS_PORT, DATABASE_URL } = process.env;
+const PRIVATE_KEY_PATH = resolve(appRootPath, 'certs', 'key.pem');
+const CERT_PATH = resolve(appRootPath, 'certs', 'cert.pem');
 const mongoStore = MongoStore.create({ mongoUrl: DATABASE_URL });
 
 export async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const server = express();
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  const httpsOptions = {
+    key: readFileSync(PRIVATE_KEY_PATH, 'utf8'),
+    cert: readFileSync(CERT_PATH, 'utf8'),
+  };
 
   app.use(helmet());
   app.use(json());
@@ -29,7 +48,10 @@ export async function bootstrap(): Promise<void> {
   app.setBaseViewsDir(resolve(__dirname, '..', 'dist'));
   app.setViewEngine('hbs');
 
-  await app.listen(PORT || 3000);
+  await app.init();
+
+  http.createServer(server).listen(PORT);
+  https.createServer(httpsOptions, server).listen(HTTPS_PORT);
 }
 
 bootstrap();
