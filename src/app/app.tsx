dfc89e1bef8cd9b8a7d356c0b57f1dec528c21f6
Lukasz Lach <lukasz.lach@getresponse.com>
2021-05-30 23:20:22 +0200

    github CI/CD test

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppRoot } from './AppRoot';
-
+console.log('test');
 ReactDOM.render(<AppRoot />, document.getElementById('app'));
